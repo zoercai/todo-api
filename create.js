6c24a8af57b5f772a452bfcad7acdb4bbc4adb7d
zoercai <zoercai@gmail.com>
2017-12-13 00:29:34 +1300

    add basic api

---
+++ create.js
@@ -0,0 +1,25 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "todos",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      todoId: data.id,
+      content: data.content,
+      lat: data.lat,
+      long: data.long,
+      createdAt: new Date().getTime()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure({ status: false }));
+  }
+}
